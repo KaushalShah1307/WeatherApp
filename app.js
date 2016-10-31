23274ae1cdab7e9c61dd24b18a4f0208692bd488
Kaushal Shah <KaushalShah1307@gmail.com>
2016-10-31 03:48:08 -0400

    Declaring 2 sets of controllers for both Home and Forecast page each

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 // Module declaration
 var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+
+// Controllers declaration
+
+weatherApp.controller ('homeController', ['$scope', function($scope){
+    
+    
+}]);
+
+weatherApp.controller ('forecastController', ['$scope', function($scope){
+    
+    
+}]);
